d24878335d72d3389dc7f8e64416ed0626de3f0b
littleLemming <twi@gmx.at>
2016-10-18 14:21:44 +0200

    Interface DAOJockey

---
+++ src/sepm/ws16/e1327450/dao/DAOJockey.java
@@ -0,0 +1,27 @@
+package sepm.ws16.e1327450.dao;
+
+import sepm.ws16.e1327450.domain.Jockey;
+import java.util.List;
+import java.sql.Date;
+
+public interface DAOJockey {
+
+    void save(Jockey j) throws PersistenceException;
+
+    Jockey load(int svnr) throws PersistenceException;
+
+    void delete(Jockey j) throws PersistenceException;
+
+    void unDelete(Jockey j) throws PersistenceException;
+
+    void update(Jockey j) throws PersistenceException;
+
+    List<Jockey> loadAll() throws PersistenceException;
+
+    List<Jockey> loadCondition(int mindKönnen, int maxKönnen, String name, Date minGeburtsdatum, Date maxGeburtsdatum, int minGewicht, int maxGewicht, boolean deleted) throws PersistenceException;
+
+    int getFreeSvnr() throws PersistenceException;
+
+    boolean isFreeSvnr(int svnr) throws PersistenceException;
+
+}
