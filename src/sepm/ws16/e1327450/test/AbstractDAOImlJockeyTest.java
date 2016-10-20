44ecbbd6d6a0df51cf1767a5f69e6aa6f5b9350a
littleLemming <twi@gmx.at>
2016-10-20 20:20:41 +0200

    Abstract Test classes for DAOJockey and DAOPferd

--- src/sepm/ws16/e1327450/test/AbstractDAOImlJockeyTest.java
+++ src/sepm/ws16/e1327450/test/AbstractDAOImlJockeyTest.java
@@ -1,4 +1,210 @@
 package sepm.ws16.e1327450.test;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sepm.ws16.e1327450.dao.DAOJockey;
+import sepm.ws16.e1327450.dao.PersistenceException;
+import sepm.ws16.e1327450.domain.Jockey;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public abstract class AbstractDAOImlJockeyTest {
+
+    final static Logger logger = LoggerFactory.getLogger(AbstractDAOImlJockeyTest.class);
+    
+    protected DAOJockey daoJockey;
+
+    protected void setDAOJockey(DAOJockey daoJockey) {
+        this.daoJockey = daoJockey;
+    }
+
+    /** load valid svnr **/
+    @Test
+    public void loadWithValid() throws PersistenceException {
+        logger.info("createWithValid()");
+        Jockey jockey = new Jockey(0,58,"Pinkie Pie",java.sql.Date.valueOf("2004-08-12"),47);
+        Jockey loadedJockey = daoJockey.load(0);
+        assertTrue(jockey.equals(loadedJockey));
+    }
+
+    /** load valid non-existing svnr **/
+    @Test
+    public void loadWithNonExisting() throws PersistenceException {
+        logger.info("loadWithNonExisting()");
+        Jockey loadedJockey = daoJockey.load(20);
+        assertTrue(loadedJockey == null);
+    }
+
+    /** load all **/
+    @Test
+    public void loadAllTest() throws PersistenceException {
+        logger.info("loadAllTest()");
+        List<Jockey> jockeyList = new ArrayList<>();
+        jockeyList.add(new Jockey(0,58,"Pinkie Pie",java.sql.Date.valueOf("2004-08-12"),47));
+        jockeyList.add(new Jockey(1,29,"Twilight Sparkle",java.sql.Date.valueOf("2003-06-07"),50));
+        jockeyList.add(new Jockey(2,301,"Rainbow Dash",java.sql.Date.valueOf("2003-05-07"),40));
+        jockeyList.add(new Jockey(3,30,"Fluttershy",java.sql.Date.valueOf("2004-01-04"),39));
+        jockeyList.add(new Jockey(4,48,"Rarity",java.sql.Date.valueOf("2003-10-11"),39));
+        jockeyList.add(new Jockey(5,287,"Princess Celestia",java.sql.Date.valueOf("1987-09-08"),61));
+        jockeyList.add(new Jockey(6,201,"Princess Luna",java.sql.Date.valueOf("1990-01-04"),59));
+        List<Jockey> jockeyListLoaded = daoJockey.loadAll();
+        for(Jockey jockey : jockeyList) {
+            assertTrue(jockeyListLoaded.contains(jockey));
+        }
+    }
+
+    /** load conditions **/
+    @Test
+    public void loadConditionTest() throws PersistenceException {
+        logger.info("loadConditionTest()");
+        List<Jockey> jockeyList = new ArrayList<>();
+        Jockey jockey0 = new Jockey(0,58,"Pinkie Pie",java.sql.Date.valueOf("2004-08-12"),47);
+        Jockey jockey1 = new Jockey(1,29,"Twilight Sparkle",java.sql.Date.valueOf("2003-06-07"),50);
+        Jockey jockey2 = new Jockey(2,301,"Rainbow Dash",java.sql.Date.valueOf("2003-05-07"),40);
+        Jockey jockey3 = new Jockey(3,30,"Fluttershy",java.sql.Date.valueOf("2004-01-04"),39);
+        Jockey jockey4 = new Jockey(4,48,"Rarity",java.sql.Date.valueOf("2003-10-11"),39);
+        Jockey jockey5 = new Jockey(5,287,"Princess Celestia",java.sql.Date.valueOf("1987-09-08"),61);
+        Jockey jockey6 = new Jockey(6,201,"Princess Luna",java.sql.Date.valueOf("1990-01-04"),59);
+        List<Jockey> jockeyListLoaded = daoJockey.loadCondition(-1,-1,null,null,null,-1,-1);
+        jockeyList = daoJockey.loadAll();
+        assertTrue(jockeyList.size() == jockeyListLoaded.size());
+        for(Jockey jockey : jockeyList) {
+            assertTrue(jockeyListLoaded.contains(jockey));
+        }
+        jockeyListLoaded = daoJockey.loadCondition(50,100,null,null,null,-1,-1);
+        jockeyList = new ArrayList<>();
+        jockeyList.add(jockey0);
+        assertTrue(jockeyList.size() == jockeyListLoaded.size());
+        for(Jockey jockey : jockeyList) {
+            assertTrue(jockeyListLoaded.contains(jockey));
+        }
+        jockeyListLoaded = daoJockey.loadCondition(-1,-1,"Princess Luna",null,null,-1,-1);
+        jockeyList = new ArrayList<>();
+        jockeyList.add(jockey6);
+        assertTrue(jockeyList.size() == jockeyListLoaded.size());
+        for(Jockey jockey : jockeyList) {
+            assertTrue(jockeyListLoaded.contains(jockey));
+        }
+        jockeyListLoaded = daoJockey.loadCondition(-1,-1,null,java.sql.Date.valueOf("2003-01-01"),java.sql.Date.valueOf("2004-01-01"),-1,-1);
+        jockeyList = new ArrayList<>();
+        jockeyList.add(jockey1);
+        jockeyList.add(jockey2);
+        jockeyList.add(jockey4);
+        assertTrue(jockeyList.size() == jockeyListLoaded.size());
+        for(Jockey jockey : jockeyList) {
+            assertTrue(jockeyListLoaded.contains(jockey));
+        }
+        jockeyListLoaded = daoJockey.loadCondition(-1,-1,null,null,null,40,60);
+        jockeyList = new ArrayList<>();
+        jockeyList.add(jockey0);
+        jockeyList.add(jockey1);
+        jockeyList.add(jockey2);
+        jockeyList.add(jockey6);
+        assertTrue(jockeyList.size() == jockeyListLoaded.size());
+        for(Jockey jockey : jockeyList) {
+            assertTrue(jockeyListLoaded.contains(jockey));
+        }
+    }
+
+    /** save with null-value **/
+    @Test(expected = IllegalArgumentException.class)
+    public void createWithNullShouldThrowException() throws PersistenceException {
+        logger.info("createWithNullShouldThrowException()");
+        daoJockey.save(null);
+    }
+
+    /** save with valid parameters **/
+    @Test
+    public void createWithValid() throws PersistenceException {
+        logger.info("createWithValid()");
+        Jockey jockey = new Jockey(7,46,"Applejack",java.sql.Date.valueOf("2005-07-04"),60);
+        List<Jockey> jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        daoJockey.save(jockey);
+        jockeyList = daoJockey.loadAll();
+        assertTrue(jockeyList.contains(jockey));
+    }
+
+    /** save with existing svnr **/
+    @Test
+    public void createWithExistingId() throws PersistenceException {
+        logger.info("createWithExistingId()");
+        Jockey jockey = new Jockey(6,46,"Applejack",java.sql.Date.valueOf("2005-07-04"),60);
+        List<Jockey> jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        daoJockey.save(jockey);
+        jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+    }
+
+    /** save and delete **/
+    @Test
+    public void deleteTest() throws PersistenceException {
+        logger.info("deleteTest()");
+        Jockey jockey = new Jockey(7,46,"Applejack",java.sql.Date.valueOf("2005-07-04"),60);
+        List<Jockey> jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        daoJockey.save(jockey);
+        jockeyList = daoJockey.loadAll();
+        assertTrue(jockeyList.contains(jockey));
+        daoJockey.delete(jockey);
+        jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+    }
+
+    /** delete with non-existing svnr **/
+    @Test
+    public void deleteNonExisting() throws PersistenceException {
+        logger.info("deleteNonExisting()");
+        Jockey jockey = new Jockey(7,46,"Applejack",java.sql.Date.valueOf("2005-07-04"),60);
+        List<Jockey> jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        daoJockey.delete(jockey);
+        jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+    }
+
+    /** save and then update **/
+    @Test
+    public void updateTest() throws PersistenceException {
+        logger.info("updateTest()");
+        Jockey jockey = new Jockey(7,46,"Applejack",java.sql.Date.valueOf("2005-07-04"),60);
+        List<Jockey> jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        daoJockey.save(jockey);
+        jockeyList = daoJockey.loadAll();
+        assertTrue(jockeyList.contains(jockey));
+        Jockey updatedJockey = new Jockey(7,55,"Applejack",java.sql.Date.valueOf("2005-07-04"),59);
+        jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        daoJockey.update(updatedJockey);
+        jockeyList = daoJockey.loadAll();
+        assertFalse(jockeyList.contains(jockey));
+        assertTrue(jockeyList.contains(jockey));
+        daoJockey.delete(updatedJockey);
+    }
+
+    /** test for free svnr **/
+    @Test
+    public void isFreeSvnrTest() throws PersistenceException {
+        logger.info("isFreeChip_NrTest()");
+        assertTrue(daoJockey.isFreeSvnr(33));
+        assertFalse(daoJockey.isFreeSvnr(2));
+    }
+
+    /** test for free chip-nr **/
+    @Test
+    public void getFreeChip_NrTest() throws PersistenceException {
+        logger.info("getFreeChip_NrTest()");
+        List<Integer> svnrList = new ArrayList<>();
+        for(Jockey jockey : daoJockey.loadAll()) {
+            svnrList.add(jockey.getSvnr());
+        }
+        Integer svnrLoaded = daoJockey.getFreeSvnr();
+        assertFalse(svnrList.contains(svnrLoaded));
+    }
+    
 }
--- src/sepm/ws16/e1327450/test/AbstractDAOImlPferdTest.java
+++ src/sepm/ws16/e1327450/test/AbstractDAOImlPferdTest.java
@@ -25,7 +25,7 @@ public abstract class AbstractDAOImlPferdTest {
     @Test
     public void loadWithValid() throws PersistenceException {
         logger.info("createWithValid()");
-        Pferd pferd = new Pferd("0000","Philidor","Trakhner",22,"0000_philidor_1.jpg",false,53,60);
+        Pferd pferd = new Pferd("0000","Philidor","Trakhner",22,"0000_philidor_1.jpg",53,60);
         Pferd loadedPferd = daoPferd.load("0000");
         assertTrue(pferd.equals(loadedPferd));
     }
@@ -40,16 +40,16 @@ public abstract class AbstractDAOImlPferdTest {
     
     /** load all **/
     @Test
-    public void loadAll() throws PersistenceException {
-        logger.info("loadAll()");
+    public void loadAllTest() throws PersistenceException {
+        logger.info("loadAllTest()");
         List<Pferd> pferdList = new ArrayList<>();
-        pferdList.add(new Pferd("0000","Philidor","Trakhner",22,"0000_philidor_1.jpg",false,53,60));
-        pferdList.add(new Pferd("0001","Rusty","Shetland Pony",16,"0001_rusty_0.jpg",false,40,46));
-        pferdList.add(new Pferd("0002","Kori","Huzule",13,"0002_kori_1.jpg",false,40,51));
-        pferdList.add(new Pferd("0003","Jaris","Fjordpony",9,"0003_jaris_2.jpg",false,56,59));
-        pferdList.add(new Pferd("0004","Pia","Haflinger",17,"0004_pia_0.jpg",false,43,55));
-        pferdList.add(new Pferd("0005","Benji","Haflinger-Noriker",15,"0005_benji_1.jpg",false,40,44));
-        pferdList.add(new Pferd("0006","Szilja","Araber",12,"0000_szilja_0.jpg",false,48,57));
+        pferdList.add(new Pferd("0000","Philidor","Trakhner",22,"0000_philidor_1.jpg",53,60));
+        pferdList.add(new Pferd("0001","Rusty","Shetland Pony",16,"0001_rusty_0.jpg",40,46));
+        pferdList.add(new Pferd("0002","Kori","Huzule",13,"0002_kori_1.jpg",40,51));
+        pferdList.add(new Pferd("0003","Jaris","Fjordpony",9,"0003_jaris_2.jpg",56,59));
+        pferdList.add(new Pferd("0004","Pia","Haflinger",17,"0004_pia_0.jpg",43,55));
+        pferdList.add(new Pferd("0005","Benji","Haflinger-Noriker",15,"0005_benji_1.jpg",40,44));
+        pferdList.add(new Pferd("0006","Szilja","Araber",12,"0000_szilja_0.jpg",48,57));
         List<Pferd> pferdListLoaded = daoPferd.loadAll();
         for(Pferd pferd : pferdList) {
             assertTrue(pferdListLoaded.contains(pferd));
@@ -58,36 +58,36 @@ public abstract class AbstractDAOImlPferdTest {
     
     /** load conditions **/
     @Test
-    public void loadCondition() throws PersistenceException {
-        logger.info("loadCondition()");
+    public void loadConditionTest() throws PersistenceException {
+        logger.info("loadConditionTest()");
         List<Pferd> pferdList = new ArrayList<>();
-        Pferd pferd0 = new Pferd("0000","Philidor","Trakhner",22,"0000_philidor_1.jpg",false,53,60);
-        Pferd pferd1 = new Pferd("0001","Rusty","Shetland Pony",16,"0001_rusty_0.jpg",false,40,46);
-        Pferd pferd2 = new Pferd("0002","Kori","Huzule",13,"0002_kori_1.jpg",false,40,51);
-        Pferd pferd3 = new Pferd("0003","Jaris","Fjordpony",9,"0003_jaris_2.jpg",false,56,59);
-        Pferd pferd4 = new Pferd("0004","Pia","Haflinger",17,"0004_pia_0.jpg",false,43,55);
-        Pferd pferd5 = new Pferd("0005","Benji","Haflinger-Noriker",15,"0005_benji_1.jpg",false,40,44);
-        Pferd pferd6 = new Pferd("0006","Szilja","Araber",12,"0000_szilja_0.jpg",false,48,57);
-        List<Pferd> pferdListLoaded = daoPferd.loadCondition(null, -1, -1, false, -1, -1, -1, -1);
+        Pferd pferd0 = new Pferd("0000","Philidor","Trakhner",22,"0000_philidor_1.jpg",53,60);
+        Pferd pferd1 = new Pferd("0001","Rusty","Shetland Pony",16,"0001_rusty_0.jpg",40,46);
+        Pferd pferd2 = new Pferd("0002","Kori","Huzule",13,"0002_kori_1.jpg",40,51);
+        Pferd pferd3 = new Pferd("0003","Jaris","Fjordpony",9,"0003_jaris_2.jpg",56,59);
+        Pferd pferd4 = new Pferd("0004","Pia","Haflinger",17,"0004_pia_0.jpg",43,55);
+        Pferd pferd5 = new Pferd("0005","Benji","Haflinger-Noriker",15,"0005_benji_1.jpg",40,44);
+        Pferd pferd6 = new Pferd("0006","Szilja","Araber",12,"0000_szilja_0.jpg",48,57);
+        List<Pferd> pferdListLoaded = daoPferd.loadCondition(null, -1, -1, -1, -1, -1, -1);
         pferdList = daoPferd.loadAll();
         assertTrue(pferdList.size() == pferdListLoaded.size());
         for(Pferd pferd : pferdList) {
             assertTrue(pferdListLoaded.contains(pferd));
         }
-        pferdListLoaded = daoPferd.loadCondition("Philidor", -1, -1, false, -1, -1, -1, -1);
+        pferdListLoaded = daoPferd.loadCondition("Philidor", -1, -1, -1, -1, -1, -1);
         pferdList.add(pferd0);
         assertTrue(pferdList.size() == pferdListLoaded.size());
         for(Pferd pferd : pferdList) {
             assertTrue(pferdListLoaded.contains(pferd));
         }
-        pferdListLoaded = daoPferd.loadCondition(null, 4, 10, false, -1, -1, -1, -1);
+        pferdListLoaded = daoPferd.loadCondition(null, 4, 10, -1, -1, -1, -1);
         pferdList = new ArrayList<>();
         pferdList.add(pferd3);
         assertTrue(pferdList.size() == pferdListLoaded.size());
         for(Pferd pferd : pferdList) {
             assertTrue(pferdListLoaded.contains(pferd));
         }
-        pferdListLoaded = daoPferd.loadCondition(null, -1, -1, false, 47, 53, -1, -1);
+        pferdListLoaded = daoPferd.loadCondition(null, -1, -1, 47, 53, -1, -1);
         pferdList = new ArrayList<>();
         pferdList.add(pferd0);
         pferdList.add(pferd6);
@@ -95,7 +95,7 @@ public abstract class AbstractDAOImlPferdTest {
         for(Pferd pferd : pferdList) {
             assertTrue(pferdListLoaded.contains(pferd));
         }
-        pferdListLoaded = daoPferd.loadCondition(null, -1, -1, false, -1, -1, 55, 60);
+        pferdListLoaded = daoPferd.loadCondition(null, -1, -1, -1, -1, 55, 60);
         pferdList = new ArrayList<>();
         pferdList.add(pferd0);
         pferdList.add(pferd3);
@@ -119,7 +119,7 @@ public abstract class AbstractDAOImlPferdTest {
     @Test
     public void createWithValid() throws PersistenceException {
         logger.info("createWithValid()");
-        Pferd pferd = new Pferd("0007","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",false,43,55);
+        Pferd pferd = new Pferd("0007","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",43,55);
         List<Pferd> pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         daoPferd.save(pferd);
@@ -131,7 +131,7 @@ public abstract class AbstractDAOImlPferdTest {
     @Test
     public void createWithExistingId() throws PersistenceException {
         logger.info("createWithExistingId()");
-        Pferd pferd = new Pferd("0005","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",false,43,55);
+        Pferd pferd = new Pferd("0005","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",43,55);
         List<Pferd> pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         daoPferd.save(pferd);
@@ -143,7 +143,7 @@ public abstract class AbstractDAOImlPferdTest {
     @Test
     public void deleteTest() throws PersistenceException {
         logger.info("deleteTest()");
-        Pferd pferd = new Pferd("0007","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",false,43,55);
+        Pferd pferd = new Pferd("0007","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",43,55);
         List<Pferd> pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         daoPferd.save(pferd);
@@ -158,7 +158,7 @@ public abstract class AbstractDAOImlPferdTest {
     @Test
     public void deleteNonExisting() throws PersistenceException {
         logger.info("deleteNonExisting()");
-        Pferd pferd = new Pferd("0008","Luis","Haflinger",7,"0008_luis_0.jpg",false,40,50);
+        Pferd pferd = new Pferd("0008","Luis","Haflinger",7,"0008_luis_0.jpg",40,50);
         List<Pferd> pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         daoPferd.delete(pferd);
@@ -170,19 +170,40 @@ public abstract class AbstractDAOImlPferdTest {
     @Test
     public void updateTest() throws PersistenceException {
         logger.info("updateTest()");
-        Pferd pferd = new Pferd("0007","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",false,43,55);
+        Pferd pferd = new Pferd("0007","Frosty","Warmblut-Mix",7,"0007_frosty_0.jpg",43,55);
         List<Pferd> pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         daoPferd.save(pferd);
         pferdList = daoPferd.loadAll();
         assertTrue(pferdList.contains(pferd));
-        Pferd updatedPferd = new Pferd("0007","Frosty","Pinto-Mix",8,"0007_frosty_0.jpg",false,45,58);
+        Pferd updatedPferd = new Pferd("0007","Frosty","Pinto-Mix",8,"0007_frosty_0.jpg",45,58);
         pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         daoPferd.update(updatedPferd);
         pferdList = daoPferd.loadAll();
         assertFalse(pferdList.contains(pferd));
         assertTrue(pferdList.contains(pferd));
+        daoPferd.delete(updatedPferd);
+    }
+
+    /** test for free chip-nr **/
+    @Test
+    public void isFreeChip_NrTest() throws PersistenceException {
+        logger.info("isFreeChip_NrTest()");
+        assertTrue(daoPferd.isFreeChip_Nr("0010"));
+        assertFalse(daoPferd.isFreeChip_Nr("0000"));
+    }
+
+    /** test for free chip-nr **/
+    @Test
+    public void getFreeChip_NrTest() throws PersistenceException {
+        logger.info("getFreeChip_NrTest()");
+        List<String> chip_nrList = new ArrayList<>();
+        for(Pferd pferd : daoPferd.loadAll()) {
+            chip_nrList.add(pferd.getChip_nr());
+        }
+        String chip_nrLoaded = daoPferd.getFreeChip_Nr();
+        assertFalse(chip_nrList.contains(chip_nrLoaded));
     }
 
 }
