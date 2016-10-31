75341395c7f210545c9934fb6c24340bc2e6739d
littleLemming <twi@gmx.at>
2016-10-31 15:26:31 +0100

    tables filled in overview

--- src/sepm/ws16/e1327450/gui/OverviewControler.java
+++ src/sepm/ws16/e1327450/gui/OverviewControler.java
@@ -1,11 +1,18 @@
 package sepm.ws16.e1327450.gui;
 
 import javafx.application.Application;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import sepm.ws16.e1327450.domain.Jockey;
+import sepm.ws16.e1327450.domain.Pferd;
+import sepm.ws16.e1327450.domain.Rennergebnis;
 
 public class OverviewControler {
 
@@ -14,51 +21,62 @@ public class OverviewControler {
     @FXML
     private Button searchJockeyButton;
     @FXML
-    private TableColumn<?, ?> rennergebnisPlatzColumn;
+    private TableColumn<Rennergebnis, Integer> rennergebnisPlatzColumn;
     @FXML
     private Button deleteButton;
     @FXML
     private Button searchPferdButton;
     @FXML
-    private TableColumn<?, ?> pferdNameColumn;
+    private TableColumn<Pferd, String> pferdNameColumn;
     @FXML
-    private TableView<?> rennergebnisTable;
+    private TableView<Rennergebnis> rennergebnisTable;
     @FXML
-    private TableColumn<?, ?> pferdChipNrColumn;
+    private TableColumn<Pferd, String> pferdChipNrColumn;
     @FXML
-    private TableColumn<?, ?> jockeyNameColumn;
+    private TableColumn<Jockey, String> jockeyNameColumn;
     @FXML
-    private TableColumn<?, ?> rennergebnisChipNrColumn;
+    private TableColumn<Rennergebnis, String> rennergebnisChipNrColumn;
     @FXML
-    private TableColumn<?, ?> rennergebnisGeschwColumn;
+    private TableColumn<Rennergebnis, Double> rennergebnisGeschwColumn;
     @FXML
-    private TableColumn<?, ?> rennergebnisRennIdColumn;
+    private TableColumn<Rennergebnis, Integer> rennergebnisRennIdColumn;
     @FXML
     private Button statistikButton;
     @FXML
-    private TableView<?> jockeyTable;
+    private TableView<Jockey> jockeyTable;
     @FXML
     private Button editButton;
     @FXML
-    private TableColumn<?, ?> jockeySvnrColumn;
+    private TableColumn<Jockey, Integer> jockeySvnrColumn;
     @FXML
-    private TableView<?> pferdTable;
+    private TableView<Pferd> pferdTable;
     @FXML
     private Button viewButton;
     @FXML
     private Button rennsimulationButton;
     @FXML
-    private TableColumn<?, ?> rennergebnisSvnrColumn;
+    private TableColumn<Rennergebnis, Integer> rennergebnisSvnrColumn;
     @FXML
     private Button searchRennergebnisButton;
 
     @FXML
     private void initialize() {
-
+        pferdChipNrColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getChip_nr()));
+        pferdNameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
+        jockeySvnrColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getSvnr()));
+        jockeyNameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
+        rennergebnisRennIdColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getRenn_id()));
+        rennergebnisChipNrColumn.setCellValueFactory(cellData -> (cellData.getValue() == null) ? new ReadOnlyStringWrapper("unknown") : new ReadOnlyStringWrapper(cellData.getValue().getPferd().getChip_nr()));
+        rennergebnisSvnrColumn.setCellValueFactory(cellData -> (cellData.getValue() == null) ? new SimpleIntegerProperty(-1).asObject() : new SimpleIntegerProperty(cellData.getValue().getJockey().getSvnr()).asObject());
+        rennergebnisGeschwColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getGeschw()));
+        rennergebnisPlatzColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getPlatz()));
     }
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
+        pferdTable.setItems(mainApp.getPferdList());
+        jockeyTable.setItems(mainApp.getJockeyList());
+        rennergebnisTable.setItems(mainApp.getRennergebnisList());
     }
 
     @FXML
