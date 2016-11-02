1bb3b58b6f026b870d725c77f548ef20a92402bd
littleLemming <twi@gmx.at>
2016-11-02 12:53:46 +0100

    refactored name

--- src/sepm/ws16/e1327450/domain/Jockey.java
+++ src/sepm/ws16/e1327450/domain/Jockey.java
@@ -5,14 +5,14 @@ import java.sql.Date;
 public class Jockey {
 
     private int svnr;
-    private double können;
+    private double koennen;
     private String name;
     private Date geburtsdatum;
     private int gewicht;
 
-    public Jockey(int svnr, double können, String name, Date geburtsdatum, int gewicht) {
+    public Jockey(int svnr, double koennen, String name, Date geburtsdatum, int gewicht) {
         this.svnr = svnr;
-        this.können = können;
+        this.koennen = koennen;
         this.name = name;
         this.geburtsdatum = geburtsdatum;
         this.gewicht = gewicht;
@@ -26,12 +26,12 @@ public class Jockey {
         this.svnr = svnr;
     }
 
-    public double getKönnen() {
-        return können;
+    public double getKoennen() {
+        return koennen;
     }
 
-    public void setKönnen(double können) {
-        this.können = können;
+    public void setKoennen(double koennen) {
+        this.koennen = koennen;
     }
 
     public String getName() {
@@ -66,7 +66,7 @@ public class Jockey {
         Jockey jockey = (Jockey) o;
 
         if (svnr != jockey.svnr) return false;
-        if (Double.compare(jockey.können, können) != 0) return false;
+        if (Double.compare(jockey.koennen, koennen) != 0) return false;
         if (gewicht != jockey.gewicht) return false;
         if (name != null ? !name.equals(jockey.name) : jockey.name != null) return false;
         return geburtsdatum != null ? geburtsdatum.equals(jockey.geburtsdatum) : jockey.geburtsdatum == null;
@@ -78,7 +78,7 @@ public class Jockey {
         int result;
         long temp;
         result = svnr;
-        temp = Double.doubleToLongBits(können);
+        temp = Double.doubleToLongBits(koennen);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
         result = 31 * result + (name != null ? name.hashCode() : 0);
         result = 31 * result + (geburtsdatum != null ? geburtsdatum.hashCode() : 0);
@@ -90,7 +90,7 @@ public class Jockey {
     public String toString() {
         return "Jockey{" +
                 "svnr=" + svnr +
-                ", können=" + können +
+                ", koennen=" + koennen +
                 ", name='" + name + '\'' +
                 ", geburtsdatum=" + geburtsdatum +
                 ", gewicht=" + gewicht +
