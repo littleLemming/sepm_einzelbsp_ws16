044b4f330cc5ea3513762c1340433bd3d2392a45
littleLemming <twi@gmx.at>
2017-02-21 18:38:17 +0100

    current status

---
+++ sql/proceedure.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS Pferd;
+DROP TABLE IF EXISTS Jockey;
+DROP TABLE IF EXISTS Rennergebnis;
+
+CREATE TABLE Pferd
+    (
+    chip_nr int NOT NULL UNIQUE,
+    name varchar(255) NOT NULL,
+    rasse varchar(255) NOT NULL,
+    alter_jahre int NOT NULL,
+    bild varchar(255) NOT NULL,
+    min_gesw double NOT NULL CHECK(40 <= min_gesw AND min_gesw <= 60),
+    max_gesw double NOT NULL CHECK(40 <= max_gesw AND max_gesw <= 60),
+    deleted boolean NOT NULL,
+    PRIMARY KEY(chip_nr)
+    );
+
+ALTER TABLE Pferd ADD CONSTRAINT gesw_constraint
+    CHECK(min_gesw <= max_gesw);
+
+CREATE TABLE Jockey
+    (
+    svnr int NOT NULL UNIQUE,
+    können double NOT NULL,
+    name varchar(255) NOT NULL,
+    geburtsdatum DATE NOT NULL,
+    gewicht int NOT NULL,
+    deleted boolean NOT NULL,
+    PRIMARY KEY(svnr)
+    );
+
+CREATE TABLE Rennergebnis
+    (
+    renn_id int NOT NULL,
+    chip_nr int NOT NULL,
+    svnr int NOT NULL,
+    dgeschw double NOT NULL,
+    pgeschw double NOT NULL,
+    glueck double NOT NULL,
+    koennen_b double NOT NULL,
+    platz int NOT NULL,
+    PRIMARY KEY(renn_id, chip_nr, svnr)
+    );
+
+ALTER TABLE Rennergebnis ADD FOREIGN KEY (chip_nr) REFERENCES Pferd(chip_nr);
+ALTER TABLE Rennergebnis ADD FOREIGN KEY (svnr) REFERENCES Jockey(svnr);
+ALTER TABLE Rennergebnis
+    ADD CONSTRAINT unique_pferd_rennen UNIQUE(renn_id,chip_nr);
+ALTER TABLE Rennergebnis
+    ADD CONSTRAINT unique_jockey_rennen UNIQUE(renn_id,svnr);
+
+INSERT INTO "PUBLIC"."PFERD" VALUES(0,'Philidor','Trakhner',22,'0000_philidor_1.jpg',53.0,60.0,false);
+INSERT INTO "PUBLIC"."PFERD" VALUES(1,'Rusty','Shetland Pony',16,'0001_rusty_0.jpg',40.0,46.0,false);
+INSERT INTO "PUBLIC"."PFERD" VALUES(2,'Kori','Huzule',13,'0002_kori_1.jpg',40.0,51.0,false);
+INSERT INTO "PUBLIC"."PFERD" VALUES(3,'Jaris','Fjordpony',9,'0003_jaris_2.jpg',56.0,59.0,false);
+INSERT INTO "PUBLIC"."PFERD" VALUES(4,'Pia','Haflinger',17,'0004_pia_0.jpg',43.0,55.0,false);
+INSERT INTO "PUBLIC"."PFERD" VALUES(5,'Benji','Haflinger-Noriker',15,'0005_benji_1.jpg',40.0,44.0,false);
+INSERT INTO "PUBLIC"."PFERD" VALUES(6,'Szilja','Araber',12,'0000_szilja_0.jpg',48.0,57.0,false);
+
+
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(0,58.0,'Pinkie Pie','2004-08-12',47,false);
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(1,29.0,'Twilight Sparkle','2003-06-07',50,false);
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(2,301.0,'Rainbow Dash','2003-05-07',40,false);
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(3,30.0,'Fluttershy','2004-01-04',39,false);
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(4,48.0,'Rarity','2003-10-11',39,false);
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(5,287.0,'Princess Celestia','1987-09-08',61,false);
+INSERT INTO "PUBLIC"."JOCKEY" VALUES(6,201.0,'Princess Luna','1990-01-04',59,false);
+
+
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(0,1,1,47.257,45.2,0.98,1.0668,2);
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(0,5,0,42.381,40.8,0.97,1.07,3);
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(0,0,6,63.663,58.7,1.01,1.073,1);
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(1,4,2,58.584,53.9,1.012,1.074,3);
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(1,3,5,64.424,58.8,1.02,1.074,1);
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(1,1,1,49.075,46.0,1.0,1.066,4);
+INSERT INTO "PUBLIC"."RENNERGEBNIS" VALUES(1,6,4,64.042,57.0,1.05,1.07,2);
---
+++ src/sepm/ws16/e1327450/gui/OverviewControler_old.java
@@ -0,0 +1,122 @@
+package sepm.ws16.e1327450.gui;
+
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import sepm.ws16.e1327450.domain.Jockey;
+import sepm.ws16.e1327450.domain.Pferd;
+import sepm.ws16.e1327450.domain.Rennergebnis;
+
+public class OverviewControler_old {
+
+    private MainApp mainApp;
+
+    @FXML
+    private Button searchJockeyButton;
+    @FXML
+    private TableColumn<Rennergebnis, Integer> rennergebnisPlatzColumn;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button searchPferdButton;
+    @FXML
+    private TableColumn<Pferd, String> pferdNameColumn;
+    @FXML
+    private TableView<Rennergebnis> rennergebnisTable;
+    @FXML
+    private TableColumn<Pferd, String> pferdChipNrColumn;
+    @FXML
+    private TableColumn<Jockey, String> jockeyNameColumn;
+    @FXML
+    private TableColumn<Rennergebnis, String> rennergebnisChipNrColumn;
+    @FXML
+    private TableColumn<Rennergebnis, Double> rennergebnisGeschwColumn;
+    @FXML
+    private TableColumn<Rennergebnis, Integer> rennergebnisRennIdColumn;
+    @FXML
+    private Button statistikButton;
+    @FXML
+    private TableView<Jockey> jockeyTable;
+    @FXML
+    private Button editButton;
+    @FXML
+    private TableColumn<Jockey, Integer> jockeySvnrColumn;
+    @FXML
+    private TableView<Pferd> pferdTable;
+    @FXML
+    private Button viewButton;
+    @FXML
+    private Button rennsimulationButton;
+    @FXML
+    private TableColumn<Rennergebnis, Integer> rennergebnisSvnrColumn;
+    @FXML
+    private Button searchRennergebnisButton;
+
+    @FXML
+    private void initialize() {
+        pferdChipNrColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getChip_nr()));
+        pferdNameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
+        jockeySvnrColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getSvnr()));
+        jockeyNameColumn.setCellValueFactory(cellData -> new ReadOnlyStringWrapper(cellData.getValue().getName()));
+        rennergebnisRennIdColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getRenn_id()));
+        rennergebnisChipNrColumn.setCellValueFactory(cellData -> (cellData.getValue() == null) ? new ReadOnlyStringWrapper("unknown") : new ReadOnlyObjectWrapper(cellData.getValue().getPferd().getChip_nr()));
+        rennergebnisSvnrColumn.setCellValueFactory(cellData -> (cellData.getValue() == null) ? new SimpleIntegerProperty(-1).asObject() : new SimpleIntegerProperty(cellData.getValue().getJockey().getSvnr()).asObject());
+        rennergebnisGeschwColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getDgeschw()));
+        rennergebnisPlatzColumn.setCellValueFactory(cellData -> new ReadOnlyObjectWrapper(cellData.getValue().getPlatz()));
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+        /*pferdTable.setItems(mainApp.getPferdList());
+        jockeyTable.setItems(mainApp.getJockeyList());
+        rennergebnisTable.setItems(mainApp.getRennergebnisList());*/
+    }
+
+    @FXML
+    void handleView() {
+
+    }
+
+    @FXML
+    void handleEdit() {
+
+    }
+
+    @FXML
+    void handleDelete() {
+
+    }
+
+    @FXML
+    void handleRennsimulation() {
+
+    }
+
+    @FXML
+    void handleStatistik() {
+
+    }
+
+    @FXML
+    void handleSearchPferd() {
+
+    }
+
+    @FXML
+    void handleSearchJockey() {
+
+    }
+
+    @FXML
+    void handleSearchRennergebnis() {
+
+    }
+
+}
